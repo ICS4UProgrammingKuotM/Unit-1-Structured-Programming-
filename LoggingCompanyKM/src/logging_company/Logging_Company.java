0bc56c69d118d1de8a203729c20b76c3ed8ef33b
KuotMariak <43444982+KuotMariak@users.noreply.github.com>
2020-02-05 13:10:50 -0500

    S

---
+++ LoggingCompanyKM/src/logging_company/Logging_Company.java
@@ -0,0 +1,26 @@
+package logging_company;
+
+public class Logging_Company {
+	
+	/*
+	 * Created by: Kuot Mariak
+	 * Created on: 05-Feb-2019
+	 * Created for: ICS4U
+	 * Day #3 (Logging Company)
+	 * Logging Company
+	*/
+	
+	public static final class ConstantMass {
+		public static final float Max_Carry_Weight = 1100;
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+	}
+
+}
