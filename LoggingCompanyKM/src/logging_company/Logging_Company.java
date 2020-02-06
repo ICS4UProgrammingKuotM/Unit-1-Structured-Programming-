ab390a6aaab5d3361080074ffe0529c231a5e337
KuotMariak <43444982+KuotMariak@users.noreply.github.com>
2020-02-06 13:10:40 -0500

    Logg

--- LoggingCompanyKM/src/logging_company/Logging_Company.java
+++ LoggingCompanyKM/src/logging_company/Logging_Company.java
@@ -11,15 +11,36 @@ public class Logging_Company {
 	*/
 	
 	public static final class ConstantMass {
-		public static final float Max_Carry_Weight = 1100;
+		public static final float Max_Carry_Weight = 1100f;	
 		
 	}
 	
+	public static final float Log ("Log_Length") {
+		public final float Log_Length1 = 0.25f;
+		public final float Log_Length2 = 0.5f;
+		public final float Log_Length3 = 1f;
+			
+	}
+	
+	public static final class Price {
+		
+		public static final float Length1_Price = 25f;
+		public static final float Length2_Price = 50f; 
+		public static final float Length3_Price = 100f;
+		
+		
+	}
+	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		float Log_Length = Log.getLog(float);
+		float Max_Weight = ConstantMass.Max_Carry_Weight;
+		float Price_Length = new
+		float Total_Price = 
 		
-		
+		float Log_Weight = 20 * Log_Length;
+		float Num_Of_Logs = Max_Weight / Log_Weight;
 		
 	}
 
