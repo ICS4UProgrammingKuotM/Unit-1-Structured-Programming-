5a35b7d75814e3039fdbf53388303f37b74a8c3d
KuotMariak <43444982+KuotMariak@users.noreply.github.com>
2020-02-26 20:33:12 -0500

    Heko
    Almost done

--- MicrowaveKM/src/microwave/Microwave.java
+++ MicrowaveKM/src/microwave/Microwave.java
@@ -1,7 +1,8 @@
 package microwave;
 
 import java.awt.EventQueue;
-import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -14,21 +15,29 @@ import javax.swing.JButton;
 import java.awt.Font;
 import java.awt.Color;
 import javax.swing.SwingConstants;
-import javax.swing.event.MouseInputAdapter;
+import javax.swing.Timer;
 
-public class Microwave {
+public class Microwave implements ActionListener{
 
 	private JFrame frmMicrowave;
 	public JLabel lblFood;
 	public JLabel lblFood1;
 	public JLabel lblFood2;
+	public JLabel lblTotalTimeANS;
+	public JLabel lblTime;
+	public JButton btnPizza;
+	public JButton btnSub;
+	public JButton btnSoup;
+	public JButton btnStart;
+	public JButton btnReset;
 	public ImageIcon MicrowaveArt = new ImageIcon("images/MicrowaveArt.png");
 	public ImageIcon Pizza = new ImageIcon("images/Pizza.png");
 	public ImageIcon Sub = new ImageIcon("images/sandwich.png");
 	public ImageIcon Soup = new ImageIcon("images/Soup.png");
 	
 	public static int count = 0;
-	public static boolean PizzaTrue = false;
+	public static double Time;
+	public static String whatFood = "";
 	
 	/**
 	 * Launch the application.
@@ -53,58 +62,77 @@ public class Microwave {
 		initialize();
 	}
 	
-	public double CalculateTotalTime (int mouseClick, boolean checkPizza ) {
+	public double CalculateTotalTime (int mouseClick,String checkItem) {
 		double PizzaTime = 45; 
 		double SubTime = 60;
 		double SoupTime = 105;
 		double TotalTime = 0;
-		boolean checkSub = false;
-		boolean checkSoup = false;
 		
-		if (checkPizza = true) {
+		
+		if (checkItem == "Pizza") {
 			if(mouseClick == 1) {
 				TotalTime = PizzaTime;
 				lblFood.setIcon(Pizza);
-				
+				lblTotalTimeANS.setText(TotalTime + " seconds");
 			}
 			else if (mouseClick == 2) {
 				TotalTime = PizzaTime * 1.5;
 				lblFood.setIcon(Pizza);
 				lblFood1.setIcon(Pizza);
+				lblTotalTimeANS.setText(TotalTime + " seconds");
 			}
 			else if (mouseClick == 3) {
 				TotalTime = PizzaTime * 2;
 				lblFood.setIcon(Pizza);
 				lblFood1.setIcon(Pizza);
 				lblFood2.setIcon(Pizza);
+				lblTotalTimeANS.setText(TotalTime + " seconds");
 			}
 			else {
 				JOptionPane.showMessageDialog(frmMicrowave, "Sorry you can only microwave 3 items at a time!");
 			}
 		}
-		else if (checkSub = true) {
+		else if (checkItem == "Sub") {
 			if(mouseClick == 1) {
 				TotalTime = SubTime;
+				lblFood.setIcon(Sub);
+				lblTotalTimeANS.setText(TotalTime + " seconds");
 			}
 			else if (mouseClick == 2) {
 				TotalTime = SubTime * 1.5;
+				lblFood.setIcon(Sub);
+				lblFood1.setIcon(Sub);
+				lblTotalTimeANS.setText(TotalTime + " seconds");
 			}
 			else if (mouseClick == 3) {
 				TotalTime = SubTime * 2;
+				lblFood.setIcon(Sub);
+				lblFood1.setIcon(Sub);
+				lblFood2.setIcon(Sub);
+				lblTotalTimeANS.setText(TotalTime + " seconds");
 			}
 			else {
 				JOptionPane.showMessageDialog(frmMicrowave, "Sorry you can only microwave 3 items at a time!");
 			}
 		}
-		else if (checkSoup = true) {
+		else if (checkItem == "Soup") {
 			if(mouseClick == 1) {
-				
+				TotalTime = SoupTime;
+				lblFood.setIcon(Soup);
+				lblTotalTimeANS.setText(TotalTime + " seconds");
 			}
 			else if (mouseClick == 2) {
-			
+				TotalTime = SoupTime * 1.5;
+				lblFood.setIcon(Soup);
+				lblFood1.setIcon(Soup);
+				lblTotalTimeANS.setText(TotalTime + " seconds");
 			}
 			else if (mouseClick == 3) {
-			
+				TotalTime = SoupTime * 2;
+				lblFood.setIcon(Soup);
+				lblFood1.setIcon(Soup);
+				lblFood2.setIcon(Soup);
+				lblTotalTimeANS.setText(TotalTime + " seconds");
 			}
 			else {
 				JOptionPane.showMessageDialog(frmMicrowave, "Sorry you can only microwave 3 items at a time!");
@@ -116,7 +144,8 @@ public class Microwave {
 		
 	}
 	
-	public void Timer(double Time) {
+	public void Timer(double cookTime) {
+		double Time = cookTime;
 		
 	}
 	
@@ -126,7 +155,42 @@ public class Microwave {
 	
 	
 	
-	
+	public void actionPerformed(ActionEvent e){
+		if(e.getSource() == btnPizza) {
+			  count++;
+		       btnSoup.setEnabled(false);
+		       btnSub.setEnabled(false);
+		       whatFood = "Pizza";
+		       Time = CalculateTotalTime(count,whatFood);
+		}
+		else if (e.getSource() == btnSub) {
+			count++;
+	        btnSoup.setEnabled(false);
+	        btnPizza.setEnabled(false);
+	        whatFood = "Sub";
+	        Time = CalculateTotalTime(count,whatFood);
+		}
+		else if (e.getSource() == btnSoup) {
+			count++;
+	        btnSub.setEnabled(false);
+	        btnPizza.setEnabled(false);
+	        whatFood = "Soup";
+	        Time = CalculateTotalTime(count,whatFood);
+		}
+		else if (e.getSource() == btnStart) {
+			Timer(Time);
+		}
+		else if (e.getSource() == btnReset) {
+			count = 0;
+			btnPizza.setEnabled(true);
+			btnSub.setEnabled(true);
+			btnSoup.setEnabled(true);
+			lblFood.setIcon(null);
+			lblFood1.setIcon(null);
+			lblFood2.setIcon(null);
+			lblTotalTimeANS.setText("");
+		}
+	}
 	
 	
 	
@@ -141,38 +205,40 @@ public class Microwave {
 		frmMicrowave.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmMicrowave.getContentPane().setLayout(null);
 		
-		JButton btnSoup = new JButton("Soup");
+		btnSoup = new JButton("Soup");
 		btnSoup.setBounds(410, 119, 81, 23);
+		btnSoup.addActionListener(this);
 		frmMicrowave.getContentPane().add(btnSoup);
 		
-		JButton btnSub = new JButton("Sub");
+		btnSub = new JButton("Sub");
 		btnSub.setBounds(410, 95, 81, 23);
+		btnSub.addActionListener(this);
 		frmMicrowave.getContentPane().add(btnSub);
 		
-		JButton btnPizza = new JButton("Pizza");
+		btnPizza = new JButton("Pizza");
 		btnPizza.setBounds(410, 70, 81, 23);
-		btnPizza.addMouseListener(new MouseAdapter(){
-		    public void mouseClicked(MouseEvent e){
-		        count++;
-		        PizzaTrue = true;
-		        CalculateTotalTime(count,PizzaTrue);
-		    }
-		});
+		btnPizza.addActionListener(this);
 		frmMicrowave.getContentPane().add(btnPizza);
 		
-		JButton btnReset = new JButton("Reset");
+		btnReset = new JButton("Reset");
 		btnReset.setBounds(410, 256, 81, 23);
+		btnReset.addActionListener(this);
 		frmMicrowave.getContentPane().add(btnReset);
 		
-		JButton btnNewButton = new JButton("Start");
-		btnNewButton.setBounds(410, 195, 65, 53);
-		frmMicrowave.getContentPane().add(btnNewButton);
+		btnStart = new JButton("Start");
+		btnStart.setBounds(410, 195, 65, 53);
+		btnStart.addActionListener(this);
+		frmMicrowave.getContentPane().add(btnStart);
 		
-		JLabel lblTotalTime = new JLabel("Total Time");
-		lblTotalTime.setBounds(410, 142, 83, 42);
+		lblTotalTimeANS = new JLabel("");
+		lblTotalTimeANS.setBounds(410, 167, 83, 32);
+		frmMicrowave.getContentPane().add(lblTotalTimeANS);
+		
+		JLabel lblTotalTime = new JLabel("Total Time:");
+		lblTotalTime.setBounds(410, 142, 65, 32);
 		frmMicrowave.getContentPane().add(lblTotalTime);
 		
-		JLabel lblTime = new JLabel("Time");
+		lblTime = new JLabel("Time");
 		lblTime.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTime.setForeground(Color.WHITE);
 		lblTime.setFont(new Font("Tahoma", Font.BOLD, 12));
@@ -180,8 +246,8 @@ public class Microwave {
 		frmMicrowave.getContentPane().add(lblTime);
 		
 		JLabel lblMicrowave = new JLabel("");
-		lblMicrowave.setIcon(new ImageIcon("N:\\Classes\\Sem2-Winter2020\\ICS4U Programming\\MaKuot (s295215)\\Daily Assignments\\Unit-1-Structured-Programming-\\MicrowaveKM\\images\\MicrowaveArt.png"));
 		lblMicrowave.setBounds(0, 0, 491, 279);
+		lblMicrowave.setIcon(MicrowaveArt);
 		frmMicrowave.getContentPane().add(lblMicrowave);
 		
 		lblFood = new JLabel("");
