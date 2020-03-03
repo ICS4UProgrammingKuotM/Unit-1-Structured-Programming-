8b2cd7d641a349d55774d8d4ffccdcdc54f0babd
KuotMariak <43444982+KuotMariak@users.noreply.github.com>
2020-03-03 13:12:13 -0500

    gifs

---
+++ RockPaperScissors!KM/src/rock_paper_scissors/RockPaperScissors.java
@@ -0,0 +1,43 @@
+package rock_paper_scissors;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class RockPaperScissors {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RockPaperScissors window = new RockPaperScissors();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public RockPaperScissors() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
